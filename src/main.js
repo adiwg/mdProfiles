c8ce55ea78aa43f4d2d5df9b4f44b96a58ea5513
Josh Bradley <jbradley@arcticlcc.org>
2019-09-16 23:04:49 -0800

    Added project and product profiles with nav

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@
 
 import full from '../resources/profiles/full.json';
 import basic from '../resources/profiles/basic.json';
+import project from '../resources/profiles/project.json';
+import product from '../resources/profiles/product.json';
 
 import schema from '../resources/profile-schema.json';
 import pkg from '../package.json';
@@ -11,7 +13,7 @@ import pkg from '../package.json';
 const version = pkg.version;
 
 function asArray() {
-  return [full, basic];
+  return [full, basic, project, product];
 }
 
-export default { full, basic, schema, asArray, version };
+export default { full, basic, project, product, schema, asArray, version };
